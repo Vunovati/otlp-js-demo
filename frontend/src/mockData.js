a25df14080356e1f559e469c22837128fc6014ed
TenaAdamic <129107111+TenaAdamic@users.noreply.github.com>
2024-03-21 16:18:46 +0100

    adding some more products and images

--- frontend/src/mockData.js
+++ frontend/src/mockData.js
@@ -5,10 +5,10 @@ export const allProducts = [
       href: '#',
       // TODO: add some sample images to the gh repo
       imageSrc:
-        'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+        'src/WebShopImages/BasicTee/BasicTee01.jpg',
       imageAlt: "Front of men's Basic Tee in black.",
       price: 35,
-      color: 'Black'
+      color: 'White'
     },
     {
       id: 2,
@@ -16,10 +16,10 @@ export const allProducts = [
       href: '#',
       // TODO: add some sample images to the gh repo
       imageSrc:
-        'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-02.jpg',
+        'src/WebShopImages/BasicTee/BasicTee06.jpg',
       imageAlt: "Front of men's Basic Tee in black.",
       price: 45,
-      color: 'Rice White'
+      color: 'White'
     },
     {
       id: 3,
@@ -27,60 +27,150 @@ export const allProducts = [
       href: '#',
       // TODO: add some sample images to the gh repo
       imageSrc:
-        'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-03.jpg',
+        'src/WebShopImages/BasicTee/BasicTee07.jpg',
       imageAlt: "Front of men's Basic Tee in black.",
       price: 37,
-      color: 'Pale Black'
+      color: 'Black'
     },
+    
     {
       id: 4,
-      name: 'Basic Tee',
+      name: 'Hoodie',
       href: '#',
       // TODO: add some sample images to the gh repo
       imageSrc:
-        'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-04.jpg',
+        'src/WebShopImages/Hoodie/hoodie01.jpg',
       imageAlt: "Front of men's Basic Tee in black.",
       price: 42,
-      color: 'Multicolor'
+      color: 'White'
     },
     // additional products...
     {
       id: 5,
-      name: 'Striped Shirt',
+      name: 'Hoodie',
       href: '#',
-      imageSrc: 'https://example.com/path/to/image.jpg',
+      imageSrc: 'src/WebShopImages/Hoodie/hoodie02.jpg',
       imageAlt: "Front of men's Striped Shirt.",
       price: 50,
-      color: 'Blue/White'
+      color: 'Orange'
     },
     {
       id: 6,
-      name: 'Denim Jeans',
+      name: 'Hoodie',
       href: '#',
-      imageSrc: 'https://example.com/path/to/image.jpg',
+      imageSrc: 'src/WebShopImages/Hoodie/hoodie03.jpg',
       imageAlt: "Front of men's Denim Jeans.",
       price: 60,
-      color: 'Blue'
+      color: 'Mint'
     },
     {
       id: 7,
       name: 'Leather Jacket',
       href: '#',
-      imageSrc: 'https://example.com/path/to/image.jpg',
-      imageAlt: "Front of men's Leather Jacket.",
+      imageSrc: 'src/WebShopImages/Leather/leather01.jpg',
+      imageAlt: "Front of woman's Leather Jacket.",
       price: 120,
-      color: 'Black'
+      color: 'Round Collar'
     },
     {
       id: 8,
+      name: 'Leather Jacket',
+      href: '#',
+      imageSrc: 'src/WebShopImages/Leather/leather02.jpg',
+      imageAlt: "Front of woman's Sneakers.",
+      price: 80,
+      color: 'Regular Collar'
+    },
+
+    {
+      id: 9,
+      name: 'Striped Shirt',
+      href: '#',
+      // TODO: add some sample images to the gh repo
+      imageSrc:
+        'src/WebShopImages/StripedTee/Striped02.jpg',
+      imageAlt: "Front of men's Basic Tee in black.",
+      price: 35,
+      color: 'Male'
+    },
+    {
+      id: 10,
+      name: 'Striped Shirt',
+      href: '#',
+      // TODO: add some sample images to the gh repo
+      imageSrc:
+        'src/WebShopImages/StripedTee/Striped03.jpg',
+      imageAlt: "Front of men's Basic Tee in black.",
+      price: 35,
+      color: 'Female'
+    },
+    {
+      id: 11,
       name: 'Sneakers',
       href: '#',
-      imageSrc: 'https://example.com/path/to/image.jpg',
-      imageAlt: "Front of men's Sneakers.",
-      price: 80,
-      color: 'Red/White'
-    }
-    // And so on, adding more products...
+      // TODO: add some sample images to the gh repo
+      imageSrc:
+        'src/WebShopImages/Sneakers/sneakers01.jpg',
+      imageAlt: "Front of men's Basic Tee in black.",
+      price: 35,
+      color: 'Blue'
+    },
+
+    {
+      id: 12,
+      name: 'Sneakers',
+      href: '#',
+      // TODO: add some sample images to the gh repo
+      imageSrc:
+        'src/WebShopImages/Sneakers/sneakers02.jpg',
+      imageAlt: "Front of men's Basic Tee in black.",
+      price: 35,
+      color: 'Red'
+    },
+    {
+      id: 13,
+      name: 'Backpack',
+      href: '#',
+      // TODO: add some sample images to the gh repo
+      imageSrc:
+        'src/WebShopImages/Backpack/backpack01.jpg',
+      imageAlt: "Front of men's Basic Tee in black.",
+      price: 35,
+      color: 'Black'
+    },
+    {
+      id: 14,
+      name: 'Backpack',
+      href: '#',
+      // TODO: add some sample images to the gh repo
+      imageSrc:
+        'src/WebShopImages/Backpack/backpack02.jpg',
+      imageAlt: "Front of men's Basic Tee in black.",
+      price: 35,
+      color: 'Gray'
+    },
+    {
+      id: 15,
+      name: 'Denim Jeans',
+      href: '#',
+      // TODO: add some sample images to the gh repo
+      imageSrc:
+        'src/WebShopImages/Jeans/jeans01.jpg',
+      imageAlt: "Front of men's Basic Tee in black.",
+      price: 35,
+      color: 'Classic Jeans'
+    },
+    {
+      id: 16,
+      name: 'Denim Jeans',
+      href: '#',
+      // TODO: add some sample images to the gh repo
+      imageSrc:
+        'src/WebShopImages/Jeans/jeans02.jpg',
+      imageAlt: "Front of men's Basic Tee in black.",
+      price: 35,
+      color: 'Ripped Jeans'
+    },
   ];
 
   
