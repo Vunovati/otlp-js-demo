77202a091b466adb3ac1be6c8bec804d753b42f8
Vladimir Adamic <vladimir.adamic@gmail.com>
2024-03-30 22:31:24 +0100

    Add products to cart through QuickView

---
+++ frontend/src/ProductQuickview.jsx
@@ -0,0 +1,146 @@
+import React, { Fragment } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import { XMarkIcon } from "@heroicons/react/24/outline";
+import { StarIcon } from "@heroicons/react/20/solid";
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
+export default function ProductQuickView({
+  product,
+  open,
+  setOpen,
+  addItemToCart,
+}) {
+  return (
+    <Transition.Root show={open} as={Fragment}>
+      <Dialog as="div" className="relative z-10" onClose={setOpen}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 hidden bg-gray-500 bg-opacity-75 transition-opacity md:block" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+          <div className="flex min-h-full items-stretch justify-center text-center md:items-center md:px-2 lg:px-4">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 md:translate-y-0 md:scale-95"
+              enterTo="opacity-100 translate-y-0 md:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 md:scale-100"
+              leaveTo="opacity-0 translate-y-4 md:translate-y-0 md:scale-95"
+            >
+              <Dialog.Panel className="flex w-full transform text-left text-base transition md:my-8 md:max-w-2xl md:px-4 lg:max-w-4xl">
+                <div className="relative flex w-full items-center overflow-hidden bg-white px-4 pb-8 pt-14 shadow-2xl sm:px-6 sm:pt-8 md:p-6 lg:p-8">
+                  <button
+                    type="button"
+                    className="absolute right-4 top-4 text-gray-400 hover:text-gray-500 sm:right-6 sm:top-8 md:right-6 md:top-6 lg:right-8 lg:top-8"
+                    onClick={() => setOpen(false)}
+                  >
+                    <span className="sr-only">Close</span>
+                    <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                  </button>
+
+                  <div className="grid w-full grid-cols-1 items-start gap-x-6 gap-y-8 sm:grid-cols-12 lg:gap-x-8">
+                    <div className="sm:col-span-4 lg:col-span-5">
+                      <div className="aspect-h-1 aspect-w-1 overflow-hidden rounded-lg bg-gray-100">
+                        <img
+                          src={product.imageSrc}
+                          alt={product.imageAlt}
+                          className="object-cover object-center"
+                        />
+                      </div>
+                    </div>
+                    <div className="sm:col-span-8 lg:col-span-7">
+                      <h2 className="text-2xl font-bold text-gray-900 sm:pr-12">
+                        {product.name}
+                      </h2>
+
+                      <section
+                        aria-labelledby="information-heading"
+                        className="mt-3"
+                      >
+                        <h3 id="information-heading" className="sr-only">
+                          Product information
+                        </h3>
+
+                        <p className="text-2xl text-gray-900">
+                          {product.price}
+                        </p>
+
+                        {/* Reviews */}
+                        <div className="mt-3">
+                          <h4 className="sr-only">Reviews</h4>
+                          <div className="flex items-center">
+                            <div className="flex items-center">
+                              {[0, 1, 2, 3, 4].map((rating) => (
+                                <StarIcon
+                                  key={rating}
+                                  className={classNames(
+                                    product.rating > rating
+                                      ? "text-gray-400"
+                                      : "text-gray-200",
+                                    "h-5 w-5 flex-shrink-0",
+                                  )}
+                                  aria-hidden="true"
+                                />
+                              ))}
+                            </div>
+                            <p className="sr-only">
+                              {product.rating} out of 5 stars
+                            </p>
+                          </div>
+                        </div>
+
+                        <div className="mt-6">
+                          <h4 className="sr-only">Description</h4>
+
+                          <p className="text-sm text-gray-700">
+                            {product.description}
+                          </p>
+                        </div>
+                      </section>
+
+                      <section
+                        aria-labelledby="options-heading"
+                        className="mt-6"
+                      >
+                        <h3 id="options-heading" className="sr-only">
+                          Product options
+                        </h3>
+
+                        <div>
+                          <div className="mt-6">
+                            <button
+                              type="submit"
+                              className="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50"
+                              onClick={() => {
+                                addItemToCart(product.id);
+                                setOpen(false);
+                              }}
+                            >
+                              Add to cart
+                            </button>
+                          </div>
+                        </div>
+                      </section>
+                    </div>
+                  </div>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  );
+}
