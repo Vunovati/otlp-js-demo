536f49e9f0f265a237cfd45f391e3caabb6f1d3b
Vladimir Adamic <vladimir.adamic@gmail.com>
2024-03-30 14:08:09 +0100

    Use vite server as cors proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,10 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/v1/traces": "http://host.docker.internal:4318", // the vite server is proxying back to host so that we skip the cors issues
+    },
+  },
   plugins: [react()],
 });
